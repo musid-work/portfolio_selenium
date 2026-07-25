01b2a5671d057201620d6402b46e10be3f442ec6
daazy <daazy_u@hotmail.com>
2026-07-24 20:40:45 -0400

    Id 840: Added login_page.py with skeleton code

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.common.by import By
+
+class SeleniumLoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.url = "https://the-internet.herokuapp.com/login"
+        self.username_input = (By.ID, "username")
+        self.password_input = (By.ID, "password")
+        self.login_button = (By.CSS_SELECTOR, "button[type='submit']")
+        self.flash_message = (By.ID, "flash")
+
+    def navigate(self):
+        self.driver.get(self.url)
+
+    def login(self, username, password):
+        self.driver.find_element(*self.username_input).send_keys(username)
+        self.driver.find_element(*self.password_input).send_keys(password)
+        self.driver.find_element(*self.login_button).click()
+
+    def get_flash_message_text(self):
+        return self.driver.find_element(*self.flash_message).text
