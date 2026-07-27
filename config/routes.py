5ec059ebed31781f0c1eac88e72325b09e29d271
daazy <daazy_u@hotmail.com>
2026-07-26 23:59:28 -0400

    Id 1159: Added config files routes and settings

---
+++ config/routes.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Routes(Enum):
+    DYNAMIC_LOADING = "/dynamic_loading/1"
+    LOGIN = "/login"
+    CHECKBOXES = "/checkboxes"
---
+++ config/settings.py
@@ -0,0 +1,5 @@
+class Settings: 
+    BASE_URL = "https://the-internet.herokuapp.com"
+    DEFAULT_TIMEOUT = 10
+    BROWSER = "chrome"
+    HEADLESS = False
