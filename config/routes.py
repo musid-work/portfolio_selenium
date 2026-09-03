d58e418fd0e7f8d9c2a86138fcc5c4c4b5af5772
daazy <daazy_u@hotmail.com>
2026-09-03 17:00:12 -0400

    090326: Added routes config file

--- config/routes.py
+++ config/routes.py
@@ -4,3 +4,5 @@ class Routes(Enum):
     DYNAMIC_LOADING = "/dynamic_loading/1"
     LOGIN = "/login"
     CHECKBOXES = "/checkboxes"
+    DROPDOWN = "/dropdown"
+    ADD_REMOVE_ELEMENTS = "/add_remove_elements/"
